f0dcb40bbcc022f6e8c819b7de50543f466cfdfd
encima <encima@gmail.com>
2016-12-28 13:12:05 +0000

    initial commit with flask backend and two way data binding on the front end (needs to update on the backend)

---
+++ backend/server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask.json import jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+heroes = [{ "id": 11, "name": 'Mr. Nice' }, { "id": 12, "name": 'Narco' }, { "id": 13, "name": 'Bombasto' }, { "id": 14, "name": 'Celeritas' }, { "id": 15, "name": 'Magneta' },  { 'id': 16, "name": 'RubberMan' },  { "id": 17, 'name': 'Dynama' },  { "id": 18, 'name': 'Dr IQ' },  { 'id': 19, 'name': 'Magma' },  { 'id': 20, 'name': 'Tornado' }];
+
+
+@app.route("/heroes/<int:id>", methods=['GET', 'OPTIONS'])
+def get_hero(id):
+    return jsonify(next((item for item in heroes if item["id"] == id), "{Error: Empty}"))
+
+@app.route("/heroes", methods=['GET', 'OPTIONS'])
+def get_heroes():
+    return jsonify(heroes)
+
+app.run(debug = True)
---
+++ src/app/hero.service.ts
@@ -0,0 +1,40 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
+
+import { HEROES } from './hero-data';
+
+@Injectable()
+export class HeroService {
+
+  private heroesUrl = 'http://localhost:5000/heroes';
+
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => {
+               console.log(response); 
+               return response.json() as Hero[]
+              })
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  getHero(id: number): Promise<Hero> {
+     const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+    .toPromise()
+    .then(response => response.json() as Hero)
+    .catch(this.handleError);
+  }
+
+}
